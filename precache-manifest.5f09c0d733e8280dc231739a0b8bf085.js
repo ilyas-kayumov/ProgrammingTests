94f51a6d4335d5ce2046ac3f78c7e02c6b2fe434
Ilyas Kayumov <i.kayumov@outlook.com>
2019-08-17 22:21:05 +0300

    Release: new questions and UI

---
+++ precache-manifest.5f09c0d733e8280dc231739a0b8bf085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8393eda0fed8a2ac9f3c5122f66f75a7",
+    "url": "/ProgrammingTests/index.html"
+  },
+  {
+    "revision": "72daae1158e7d64aba5e",
+    "url": "/ProgrammingTests/static/css/main.7fba1201.chunk.css"
+  },
+  {
+    "revision": "8cc2c7d0256d980a0f94",
+    "url": "/ProgrammingTests/static/js/2.7a168835.chunk.js"
+  },
+  {
+    "revision": "72daae1158e7d64aba5e",
+    "url": "/ProgrammingTests/static/js/main.1cc04e9b.chunk.js"
+  },
+  {
+    "revision": "b42cfc029f290157806e",
+    "url": "/ProgrammingTests/static/js/runtime~main.26a331af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff5b109b2dcd02475c571fa441c75981.js"
+  "/ProgrammingTests/precache-manifest.5f09c0d733e8280dc231739a0b8bf085.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ProgrammingTests/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
